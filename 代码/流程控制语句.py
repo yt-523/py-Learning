5ac090d14103c78e53d737c2c6866f23a8f5e580
yt-523 <omarf6@gmail.com>
2026-09-12 21:33:44 +0800

    更新match。。。case部分

--- 代码/流程控制语句.py
+++ 代码/流程控制语句.py
@@ -40,7 +40,7 @@ print("---------------------------------")
 a = int(input('请输入三角形的第一条边：'))
 b = int(input('请输入三角形的第二条边：'))
 c = int(input('请输入三角形的第三条边：'))
-if a + b <= c or a + c <= b and b + c <= a:
+if a + b <= c or a + c <= b or b + c <= a:
     print('不能构成三角形')
 elif a == b and b == c:
     print('等边三角形')
@@ -62,3 +62,33 @@ else:
 
 print("---------------------------------")
 
+day =input("今天星期几")
+match day:
+    case "1":
+        print("工作")
+    case "2":
+        print("出差")
+    case "3"|"4":
+        print("加班")
+    case "5":
+        print("采购")
+    case "6"|"7":
+        print("休息")
+    case _:
+        print("输入错误")
+
+#小案例:简易计算器
+num1 = float(input("请输入第一个数字: "))
+oper = input("请输入运算符 (+, -, *, /): ")
+num2 = float(input("请输入第二个数字: "))
+match oper:
+    case "+":
+        print(f"{num1} + {num2} = {num1 + num2}")
+    case "-":
+        print(f"{num1} - {num2} = {num1 - num2}")
+    case "*":
+        print(f"{num1} * {num2}= {num1 * num2}")
+    case "/"if num2 != 0:
+        print(f"{num1} / {num2} = {num1 / num2}")
+    case _:
+        print("无效的运算")
